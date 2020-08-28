76f5eb4c4471c701c1a115d3915096cf566d6423
Roberto Orellana <rorepoid@gmail.com>
2020-08-27 23:58:58 -0500

    style: Remove tag close (#5)

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react";
 import ErrorPage from "../components/ErrorPage";
 
 const NotFoundPage = () => (
-  <ErrorPage errorCode="404" message="Nos perdimos❓"></ErrorPage>
+  <ErrorPage errorCode="404" message="Nos perdimos❓" />
 );
 
 export default NotFoundPage;
