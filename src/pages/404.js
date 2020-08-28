8035779a6b2d97b1b1fa47e7df618ae3e86f0aa5
Roberto Orellana <rorepoid@gmail.com>
2020-08-27 23:00:16 -0500

    feat: Add 404 error page (#5)

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ErrorPage from "../components/ErrorPage";
+
+const NotFoundPage = () => (
+  <ErrorPage errorCode="404" message="Nos perdimos❓"></ErrorPage>
+);
+
+export default NotFoundPage;
